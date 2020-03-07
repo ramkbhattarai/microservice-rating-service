802cf332ea4098a9e716eb7da9513413e0c04c1a
Ram <ramkb466@gmail.com>
2020-03-07 08:57:13 -0500

    changed to accept api call from others

---
+++ src/main/java/com/rkb/models/UserRating.java
@@ -0,0 +1,19 @@
+package com.rkb.models;
+
+import java.util.List;
+
+public class UserRating {
+	
+	private List<Rating> userRatings;
+
+	public List<Rating> getUserRatings() {
+		return userRatings;
+	}
+
+	public void setUserRatings(List<Rating> userRatings) {
+		this.userRatings = userRatings;
+	}
+	
+	
+
+}
