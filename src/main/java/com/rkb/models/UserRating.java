588a04f40d50bd0a3e8ae8044d2f49389b4a525b
Ram <ramkb466@gmail.com>
2020-03-10 13:02:22 -0400

    added api call

--- src/main/java/com/rkb/models/UserRating.java
+++ src/main/java/com/rkb/models/UserRating.java
@@ -1,10 +1,20 @@
 package com.rkb.models;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class UserRating {
+	 private String userId;
 	
 	private List<Rating> userRatings;
+	
+	 public String getUserId() {
+	        return userId;
+	    }
+
+	    public void setUserId(String userId) {
+	        this.userId = userId;
+	    }
 
 	public List<Rating> getUserRatings() {
 		return userRatings;
@@ -14,6 +24,12 @@ public class UserRating {
 		this.userRatings = userRatings;
 	}
 	
-	
+	 public void initData(String userId) {
+	        this.setUserId(userId);
+	        this.setUserRatings(Arrays.asList(
+	                new Rating("100", 3),
+	                new Rating("200", 4)
+	        ));
+	    }
 
 }
